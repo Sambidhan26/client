3a026065a72a8ca632520af9957539c094bf34a5
Sambidhan26 <sambidhan26@gmail.com>
2026-03-08 11:15:30 +0800

    Angular Setup done: using typescript frontend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import 'zone.js';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideZoneChangeDetection(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ]
 };
